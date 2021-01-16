505fcd70bc680c9f0c4dd22d54feece13a86dd77
Jeka8833 <eadercach@gmail.com>
2021-01-17 00:08:08 +0200

    Update

--- src/SerialControl.cpp
+++ src/SerialControl.cpp
@@ -5,39 +5,31 @@
 #include "SerialControl.h"
 #include "FanControl.h"
 
-const uint8_t setSpeedCmd = 's';
-const uint8_t checkAlive = 'g';
-
 void SerialControl::init() {
     Serial.begin(9600);
 }
 
 void SerialControl::update() {
-    if (Serial.available()) {
-        uint8_t mode = Serial.read();
-        if (mode == setSpeedCmd) {
-            uint8_t fan1Mode = read();
-            if (fan1Mode == 0x55)
-                FanControl::fan1Speed = 0xFFFF;
-            else if (fan1Mode == 0xCC)
-                FanControl::fan1Speed = read();
-            else
-                return;
+    if (read() == 0xC0) {
+        uint8_t fan1Mode = read();
+        if (fan1Mode == 0x55)
+            FanControl::fan1Speed = 0xFFFF;
+        else if (fan1Mode == 0xCC)
+            FanControl::fan1Speed = read();
+        else
+            return;
 
-            uint8_t fan2Mode = read();
-            if (fan2Mode == 0x55)
-                FanControl::fan2Speed = 0xFFFF;
-            else if (fan2Mode == 0xCC)
-                FanControl::fan2Speed = read();
-            else
-                return;
+        uint8_t fan2Mode = read();
+        if (fan2Mode == 0x55)
+            FanControl::fan2Speed = 0xFFFF;
+        else if (fan2Mode == 0xCC)
+            FanControl::fan2Speed = read();
+        else
+            return;
 
-            FanControl::fan0Speed = read();
-            FanControl::update();
-            Serial.println("Good");
-        } else if (mode == checkAlive) {
-            Serial.write(0xFF);
-        }
+        FanControl::fan0Speed = read();
+        FanControl::update();
+        Serial.write(0xFF);
     }
 }
 
